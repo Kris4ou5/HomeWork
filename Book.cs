3883f530031e7f64a41cf4b77070be6a76909b88
Kris4ou5 <krisiivanov08@gmail.com>
2025-10-27 01:07:01 +0200

    Add files via upload

--- Book.cs
+++ Book.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace ClassBookUPR
 {
-    internal class Book
+    public class Book
     {
+        
+            private string title;
+            private string author;
+            private decimal price;
+
+            public string Title
+            {
+                get { return title; }
+                set
+                {
+                    if (string.IsNullOrWhiteSpace(value) || value.Length < 3)
+                    {
+                        throw new ArgumentException("Заглавието трябва да е поне 3 символа.");
+                    }
+                    title = value;
+                }
+            }
+
+            public string Author
+            {
+                get { return author; }
+                set
+                {
+                    if (string.IsNullOrWhiteSpace(value))
+                    {
+                        throw new ArgumentException("Името на автора не може да е празно.");
+                    }
+                    author = value;
+                }
+            }
+
+            public decimal Price
+            {
+                get { return price; }
+                set
+                {
+                    if (value < 0)
+                    {
+                        throw new ArgumentException("Цената не може да бъде отрицателна.");
+                    }
+                    price = value;
+                }
+            }
+
+            public Book(string title, string author, decimal price)
+            {
+                Title = title;
+                Author = author;
+                Price = price;
+            }
+
+        
     }
 }
